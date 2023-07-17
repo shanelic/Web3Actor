10bec32ca17cac21e4c47132fb777261445ae1f9
Shane @ Red Building Labs <shanelic@rblabs.io>
2023-07-17 17:45:21 +0800

    core: implement Web3.swift

--- Package.swift
+++ Package.swift
@@ -9,20 +9,30 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Web3Actor",
-            targets: ["Web3Actor"]),
+            targets: ["Web3Actor"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Boilertalk/Web3.swift.git", from: "0.6.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Web3Actor",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Web3", package: "Web3.swift"),
+                .product(name: "Web3PromiseKit", package: "Web3.swift"),
+                .product(name: "Web3ContractABI", package: "Web3.swift"),
+            ]
+        ),
         .testTarget(
             name: "Web3ActorTests",
-            dependencies: ["Web3Actor"]),
+            dependencies: [
+                "Web3Actor",
+            ]
+        ),
     ]
 )
