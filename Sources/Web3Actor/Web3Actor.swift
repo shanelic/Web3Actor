45e3b1f411246dc61a8d0e08cc4ba720e33a760c
Shane @ Red Building Labs <shanelic@rblabs.io>
2023-07-31 16:02:43 +0800

    feat: initializer for api keys in (implicit) helper
    for non-isolated usage

--- Sources/Web3Actor/Web3Actor.swift
+++ Sources/Web3Actor/Web3Actor.swift
@@ -2,4 +2,15 @@
 actor Web3Actor {
     static var shared = Web3Actor()
     private init() {}
+    
+    public func initializeApis(openseaApiKey: String? = nil, etherscanApiKey: String? = nil) {
+        ActorHelper.shared.openseaApiKey = openseaApiKey
+        ActorHelper.shared.etherscanApiKey = etherscanApiKey
+    }
+}
+
+class ActorHelper {
+    static var shared = ActorHelper()
+    var openseaApiKey: String!
+    var etherscanApiKey: String!
 }
