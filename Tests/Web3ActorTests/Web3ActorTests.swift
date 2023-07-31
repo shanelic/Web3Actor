c99c3bb47c7b16e4d971b5eed292abd4100eb833
Shane @ Red Building Labs <shanelic@rblabs.io>
2023-07-31 16:29:41 +0800

    test: redefine example test script

--- Tests/Web3ActorTests/Web3ActorTests.swift
+++ Tests/Web3ActorTests/Web3ActorTests.swift
@@ -2,10 +2,11 @@ import XCTest
 @testable import Web3Actor
 
 final class Web3ActorTests: XCTestCase {
-    func testExample() throws {
+    func testExample() async throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Web3Actor().text, "Hello, World!")
+        let contracts = await Web3Actor.shared.getContracts()
+        XCTAssertEqual(contracts, [])
     }
 }
