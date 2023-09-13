44b60d7087324608e32731362f82f91f3068ef81
shanelic_ <me@shanelic.io>
2023-09-13 22:27:33 +0800

    test: get NFTs

--- Tests/Web3ActorTests/Web3ActorTests.swift
+++ Tests/Web3ActorTests/Web3ActorTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import Web3
 @testable import Web3Actor
 
 final class Web3ActorTests: XCTestCase {
@@ -8,5 +9,14 @@ final class Web3ActorTests: XCTestCase {
         // results.
         let contracts = await Web3Actor.shared.getContracts()
         XCTAssertEqual(contracts, [])
+    }
+    
+    func testOpensea() async throws {
+        await Web3Actor.shared.initialize(.EthereumGoerli, openseaApiKey: "c823fdee93814f7abd5492604697e9c8")
+        guard let address = EthereumAddress(hexString: "0x85fD692D2a075908079261F5E351e7fE0267dB02") else { return }
+        var cursor = try await Web3Actor.shared.getNFTs(of: address)
+        while cursor != nil {
+            cursor = try await Web3Actor.shared.getNFTs(of: address, nextCursor: cursor)
+        }
     }
 }
